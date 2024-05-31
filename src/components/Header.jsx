82f28776c9ac83a30ad066944d09ac114b1bb66d
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-05-31 08:09:47 +0200

    Added jsdocs on header componet

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+/**
+ *
+ * @returns {JSX.Element}
+ */
+export default function Header() {
+  {
+    return (
+      <header className="header">
+        <img className="header--image" src="./images/react-icon-small.png" />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project"> React Course - Project 3</h4>
+      </header>
+    );
+  }
+}
